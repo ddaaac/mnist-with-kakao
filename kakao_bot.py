ffa1ac3edddc5c0b5ebb9b32b1e8f28533708248
Beom Jun <skyblue300a@gmail.com>
2018-11-30 23:59:27 +0900

    using flask

---
+++ kakao_bot.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+import json
+import urllib.request
+import mnist
+import pickle
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+
+@app.route("/keyboard")
+def keyboard():
+        content = {
+            'type' : 'text',
+            }
+        return jsonify(content)
+
+@app.route("/message",methods=['GET', 'POST'])
+def message():
+        #data = request.get_json()
+        data = json.loads(request.data)
+        img_url = data['content']
+        print(img_url)
+        
+        urllib.request.urlretrieve(img_url, './test.png')
+      
+        result = mnist.mnist()
+        
+        response ={
+                "message" :{
+                        "text" : result
+                }
+        }
+
+        response = json.dumps(response)
+
+        return response
+
+if __name__ == "__main__":
+    #myProcessing._setup_()
+    app.run(host="0.0.0.0", port=5000)
